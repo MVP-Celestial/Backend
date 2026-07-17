0f0ae816cd1081e18f908e6c3f68ae5353d94839
Celestial <celestialcreator11@gmail.com>
2026-07-17 17:48:43 +0530

    email transporter bug fixed

--- 103_Perplexity/src/controller/auth.controller.js
+++ 103_Perplexity/src/controller/auth.controller.js
@@ -1,5 +1,7 @@
 import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import { sendEmail } from "../services/mail.service.js";
+
 
 export async function register(req, res) {
 
@@ -18,6 +20,26 @@ export async function register(req, res) {
         })
     }
 
-    const user = await userModel.create
+    const user = await userModel.create({ username, email, password })
+
+    await sendEmail({
+        to: email,
+        subject: "Welcome to perplexity",
+        text: `Hi ${username}, \n\n Thankyou for registering at perplexity. We are excited to have you on board\n\nBest regards,\nThe perplexity Team`,
+        html: `<p>Hi ${username}, </p>
+        <p>Thankyou for registering at <strong>Perplexity</strong>. We're excited to have you on board</p>
+        <p>Best regards, <br>The perplexity team</br></p>`
+    })
+
+    res.status(201).json({
+        message: "user registered successfully",
+        success: true,
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+        }
+
+    })
 
 }
--- 103_Perplexity/src/services/mail.service.js
+++ 103_Perplexity/src/services/mail.service.js
@@ -1,8 +1,12 @@
 import nodemailer from "nodemailer"
 
+//transporter connects smtp server with webserver
+
+
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
+        type: "OAuth2",
         user: process.env.GOOGLE_USER,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
@@ -12,4 +16,17 @@ const transporter = nodemailer.createTransport({
 
 transporter.verify()
 .then(()=>{console.log("email transporter is ready to send emails");})
-.catch((err)=>{console.log('email transporter verification failed');})
+.catch((err)=>{console.log('email transporter verification failed:', err);})
+
+export async function sendEmail({to, subject, html, text}) {
+    const mailOptions = {
+        from: process.env.GOOGLE_USER,
+        to,
+        subject,
+        html,
+        text
+    };
+
+
+
+}
