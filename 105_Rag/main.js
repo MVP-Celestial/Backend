819210a32717adb25ee179b833c5363ccb690919
Celestial <celestialcreator11@gmail.com>
2026-09-14 18:06:48 +0530

    RAG PDF-Parser

---
+++ 105_Rag/main.js
@@ -0,0 +1,15 @@
+import { PDFParse } from "pdf-parse";
+import fs from "fs";
+
+let dataBuffer = fs.readFileSync("./QT.pdf");
+
+const parser = new PDFParse({
+    data: dataBuffer,
+});
+
+const data = await parser.getText();
+
+console.log(data);
+
+
+
