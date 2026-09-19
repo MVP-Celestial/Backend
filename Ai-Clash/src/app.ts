e57db7d1670e115e946c7799ebe18dcf3670c2bf
Celestial <celestialcreator11@gmail.com>
2026-09-19 18:27:12 +0530

    States setup

--- Ai-Clash/src/app.ts
+++ Ai-Clash/src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import useGraph from './services/graph.ai.service.js'
+
 
 const app = express();
-
 
 
 app.get('/health', (req, res)=>{
     res.status(200).json({message: "Server is running"})
 });
+
+app.post("/use-graph",async (req, res)=> {
+   await useGraph("What is the capital of France ?") 
+})
 
 export default app;
--- Ai-Clash/src/services/graph.ai.service.ts
+++ Ai-Clash/src/services/graph.ai.service.ts
@@ -1,31 +1,32 @@
-import { StateSchema, MessagesValue, type GraphNode, StateGraph, START, END } from "@langchain/langgraph";
+import { StateSchema, MessagesValue, StateGraph, START, END } from "@langchain/langgraph";
+import type { GraphNode } from "@langchain/langgraph";
+import { HumanMessage } from "@langchain/core/messages";
 
 
-
-type JUDGEMENT = {
-    winner: "solution_1" | "solution_2";
-    solution_1_score: number;
-    solution_2_score: number;
-}
-
-
-
-type AIBATTLESTATE =  {
-    messages: typeof MessagesValue,
-    solution_1: string,
-    solution_2: string,
-    judgement: JUDGEMENT
-
-}
-
-const state: AIBATTLESTATE = { //transfer data from one node to another using this state.
+const State = new StateSchema({
     messages: MessagesValue,
-    solution_1: "",
-    solution_2: "",
-    judgement: {
-        winner: "solution_1",
-        solution_1_score: 0,
-        solution_2_score: 0
-    }
+});
 
+const solutionNode: GraphNode<typeof State> = (state) => {
+
+    console.log(state.messages);
+    return {
+        messages: state.messages
+    }
 }
+
+const graph = new StateGraph(State)
+      .addNode("solution", solutionNode)
+      .addEdge(START, "solution")
+      .addEdge("solution", END)
+      .compile();
+
+      export default async function(userMessage:string) {
+        const result = await graph.invoke({
+            messages:[
+                new HumanMessage(userMessage)
+            ]
+        })
+
+        return result.messages
+      }
